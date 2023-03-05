b201c6664f9d207d204bfdd0a82d8be0a3b6de9d
anetryg <58123887+anetryg@users.noreply.github.com>
2023-03-05 15:27:09 +0100

    cv_3

--- cv3/arrayMethods.js
+++ cv3/arrayMethods.js
@@ -1,3 +1,15 @@
+// 1) Napište funkci, která otočí pořadí prvků ve vrstupním poli. Nepoužívejte metodu reverse().
+
+
+// 2) Napište funkci, která převede pole hodnot z mil na kilometry pomocí metody mapy. Nakonec sečtěte kilometry v nové proměnné s názvem "totalDistanceInKilometers" a vraťte tuto proměnnou.
+
+
+// 3) Napište funkci nazvanou "cleanNames", která přijímá pole řetězců obsahujících další mezery na začátku a konci. Funkce cleanNames() by měla použít metodu map() k vrácení nového pole s oříznutými názvy.
+
+
+// 4) Napište funkci, který vybere z pole pouze sudá čísla pomocí metody filer() a poté je umocněte
+
+
 const orders = [
     { orderId: '123', customerId: '123', deliveryDate: '01-01-2020', delivered: true, items: [
         { productId: '123', price: 55 },
@@ -20,16 +32,16 @@ const orders = [
     ]},
 ];
 
-// 1) Získejte list objednávek zákazníka 234, ktoré nebyly doručené
+// 5) Získejte list objednávek zákazníka 234, které nebyly doručené
 
 
-// 2) Pro každou objednávku vytvořte nový atribut 'total' s celkovou cenou za všechny objednané produkty
+// 6) Pro každou objednávku vytvořte nový atribut 'total' s celkovou cenou za všechny objednané produkty
 
 
-// 3) Na max 1 řádek: Bly všechny objednávky doručené?
+// 7) Na max 1 řádek: Byly všechny objednávky doručené?
 
 
-// 4) Na max 1 řádek: Objednal si něco zákazník s id 123?
+// 8) Na max 1 řádek: Objednal si něco zákazník s id 123?
 
 
-// 5) Na max 1 řádek:: Byl objednaný produkt s id 123?
+// 9) Na max 1 řádek:: Byl objednaný produkt s id 123?
